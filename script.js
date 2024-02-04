076dacfea933b050c27487ec3f1a15e09e136dcc
rtwalsh <rwalsh@excalibur-solutions.com>
2024-02-04 18:35:07 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,94 @@
+/*
+ *  Password Manager
+ *
+ *  Author: Robert Walsh
+ *  Date:    February 4, 2024
+ *
+ */
+
+const MIN_PASSPHRASE_LENGTH = 4;
+
+let configurePage = {};
+let mainPage = {};
+let unlockDlg = {};
+let newEntryDlg = {};
+
+/////////////////////////////////////////////////
+// Main Page Functions
+/////////////////////////////////////////////////
+
+function initMainPage() {
+  
+}
+
+function signOut() {
+  
+}
+
+function enableSearch(event) {
+  
+}
+
+function search() {
+  
+}
+
+function addNew() {
+  
+}
+
+function revealPassword(event) {
+  
+}
+
+function editPassword(event) {
+  
+}
+
+function deletePassword(event) {
+  
+}
+
+/////////////////////////////////////////////////
+// Main Page : Unlock Dialog Functions
+/////////////////////////////////////////////////
+
+function enableUnlock(event) {
+  
+}
+
+function unlock(event) {
+  event.preventDefault();
+}
+
+/////////////////////////////////////////////////
+// Main Page : Add New Entry Dialog Functions
+/////////////////////////////////////////////////
+
+function enableAddNewEntry() {
+  
+}
+
+function addNewEntry(event) {
+  
+}
+
+function cancelAddNewEntry(event) {
+  
+}
+
+/////////////////////////////////////////////////
+// Configure Page Functions
+/////////////////////////////////////////////////
+
+function initConfigurePage() {
+
+}
+
+function enableSetPassphrase() {
+
+}
+
+function setPassphrase(event) {
+
+}
